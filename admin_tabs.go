d0aa510b479f4d39f7a622be15b5a8a1f206105b
Moises P. Sena <moisespsena@gmail.com>
2018-08-24 09:55:11 -0300

    Add sources

--- admin_tabs.go
+++ admin_tabs.go
@@ -1,1 +1,56 @@
 package admin_tabs
+
+import (
+	"github.com/aghape/admin"
+	"github.com/aghape/core"
+	"github.com/aghape/core/utils"
+	"github.com/moisespsena-go/aorm"
+	"github.com/moisespsena/go-path-helpers"
+)
+
+var (
+	PKG      = path_helpers.GetCalledDir()
+	KEY_TABS = PKG + ".tabs"
+	KEY_TAB  = PKG + ".tab"
+	THEME    = "tabbed"
+)
+
+func PrepareResource(res *admin.Resource, tabs Tabs, defaultTab *Tab) {
+	index := res.Router.FindHandler("GET", admin.P_INDEX).(*admin.RouteHandler)
+	indexHandler := index.Handle
+
+	if defaultTab != nil {
+		index.Handle = func(c *admin.Context) {
+			c.Data().Set(KEY_TAB, defaultTab)
+			indexHandler(c)
+		}
+	}
+
+	scopesMap := &TabsData{tabs, make(map[string]*Tab)}
+
+	for _, scope := range tabs {
+		if scope.Path == "" {
+			scope.Path = utils.ToParamString(scope.Title)
+		}
+		if scope.TitleKey == "" {
+			scope.TitleKey = res.I18nPrefix + ".tabs." + scope.Path
+		}
+		scopesMap.ByPath[scope.Path] = scope
+	}
+
+	res.Data.Set(KEY_TABS, scopesMap)
+
+	for _, tab := range tabs {
+		res.Router.Get("/"+tab.Path, TabHandler(res, index.Config, indexHandler, tab))
+	}
+
+	res.DefaultFilter(func(context *core.Context, db *aorm.DB) *aorm.DB {
+		scopePath := GetTabPath(context)
+		if scope, ok := scopesMap.ByPath[scopePath]; ok {
+			return scope.Handler(res, context, db)
+		}
+		return db
+	})
+
+	res.UseTheme(THEME)
+}
---
+++ tabs.go
@@ -0,0 +1,52 @@
+package admin_tabs
+
+import (
+	"github.com/aghape/admin"
+	"github.com/aghape/core"
+	"github.com/moisespsena-go/aorm"
+)
+
+type Tab struct {
+	Title    string
+	Path     string
+	TitleKey string
+	Handler  func(res *admin.Resource, context *core.Context, db *aorm.DB) *aorm.DB
+	Default  bool
+}
+
+func (s *Tab) URL(res *admin.Resource, context *core.Context) string {
+	if s.Default {
+		return res.GetContextIndexURI(context)
+	}
+	return res.GetContextIndexURI(context) + "/" + s.Path
+}
+
+type Tabs []*Tab
+
+type TabsData struct {
+	Tabs   Tabs
+	ByPath map[string]*Tab
+}
+
+func GetTabPath(context *core.Context) string {
+	if scope, ok := context.Data().GetOk(KEY_TAB); ok {
+		return scope.(*Tab).Path
+	}
+	return ""
+}
+
+func GetTab(context *core.Context) *Tab {
+	if tab, ok := context.Data().GetOk(KEY_TAB); ok {
+		return tab.(*Tab)
+	}
+	return nil
+}
+
+func TabHandler(res *admin.Resource, config *admin.RouteConfig, indexHandler admin.Handler, scope *Tab) *admin.RouteHandler {
+	return admin.NewHandler(func(c *admin.Context) {
+		c.Breadcrumbs().Append(core.NewBreadcrumb(res.GetContextIndexURI(c.Context), res.GetLabelKey(true), ""))
+		c.Data().Set("page_title", c.T(scope.TitleKey, scope.Title))
+		c.Data().Set(KEY_TAB, scope)
+		indexHandler(c)
+	}, config)
+}
